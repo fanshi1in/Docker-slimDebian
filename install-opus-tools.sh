30432de474cd7673ab03a28ffe892306aaa6f499
fanshi1in <61900994+fanshi1in@users.noreply.github.com>
2021-05-10 15:08:17 +0800

    Add fffmpeg support

--- install-opus-tools.sh
+++ install-opus-tools.sh
@@ -30,20 +30,20 @@ curl -Ls https://archive.mozilla.org/pub/opus/libopusenc-0.2.1.tar.gz | tar xz -
 # Opus Tools 0.2
 curl -Ls https://archive.mozilla.org/pub/opus/opus-tools-0.2.tar.gz | tar xz -C "$TEMP_FOLDER"
 
+# ffmpeg 4.4
+curl -Ls https://ffmpeg.org/releases/ffmpeg-4.4.tar.gz | tar xz -C "$TEMP_FOLDER"
+
 # Compile
 
 cd "$TEMP_FOLDER"/opus-1.3.1 || exit
-
 ./configure
 make && make install
 
 cd "$TEMP_FOLDER"/opusfile-0.11 || exit
-
 ./configure
 make && make install
 
 cd "$TEMP_FOLDER"/libopusenc-0.2.1 || exit
-
 ./configure
 make && make install
 
@@ -52,7 +52,10 @@ cd "$TEMP_FOLDER"/opus-tools-0.2 || exit
 make
 make install
 ldconfig
-
 cp "$TEMP_FOLDER"/opus-tools-0.2/opusrtp /usr/local/bin
+
+cd "$TEMP_FOLDER"/ffmpeg-4.4 || exit
+./configure --disable-debug --enable-static --enable-dts -enable-libopus --enable-encoder=libopus
+make && make install
 
 rm -rf "$TEMP_FOLDER"
